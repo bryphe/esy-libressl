30049e17205ca7dcfe6487dacb2de6bfe6490fb0
Bryan Phelps <bryphe@outlook.com>
2018-10-19 17:35:49 -0700

    Include winsock libraries for windows

--- esy/test.sh
+++ esy/test.sh
@@ -6,8 +6,10 @@ INCLUDE=$cur__install/include
 
 if which x86_64-w64-mingw32-gcc; then
     CC=x86_64-w64-mingw32-gcc
+    ADDITIONAL_LIBS="-lws2_32 -lwsock32"
 else
     CC=gcc
+    ADDITIONAL_LIBS=-lpthread
 fi
 
 echo "Using compiler: $CC"
@@ -17,7 +19,7 @@ ls -a $INCLUDE
 echo "lib.."
 ls -a $cur__lib
 
-$CC ./../esy/test.c -o ./test.exe -I$INCLUDE -L$cur__lib -lssl -lcrypto -lpthread
+$CC ./../esy/test.c -o ./test.exe -I$INCLUDE -L$cur__lib -lssl -lcrypto $ADDITIONAL_LIBS
 
 export PATH=$PATH:$cur__bin:$cur__lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$cur__lib
