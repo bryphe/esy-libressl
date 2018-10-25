b945b6aef3291613b0bdda89c383a7bfa102797a
Bryan Phelps <bryphe@outlook.com>
2018-10-25 14:09:08 -0700

    Check for existing libssl.a

--- esy/build.sh
+++ esy/build.sh
@@ -1,9 +1,15 @@
 cd _build
 
+echo "Checking for existing SSL installs..."
 ls -a /lib/libssl*
 ls -a /usr/lib/libssl*
 
 exit 1
 
-# make VERBOSE=1
-# make install VERBOSE=1
+if [ -e /usr/lib/libssl.a ] then
+    echo "libssl.a already available; not building"
+else
+    make VERBOSE=1
+    make install VERBOSE=1
+fi
+
